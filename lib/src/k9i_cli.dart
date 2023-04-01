4004811f7adbec5cbd645a94b2859b045f89ca25
K9i-0 <geukfyger@gmail.com>
2023-04-01 20:51:50 +0900

    addServeWebTask addUpdatePodsTask

--- lib/src/k9i_cli.dart
+++ lib/src/k9i_cli.dart
@@ -1,10 +1,18 @@
+import 'dart:io';
+
 import 'package:grinder/grinder.dart';
 import 'package:k9i_cli/src/utils.dart';
 
+/// Add all tasks from the k9i_cli package.
+///
+/// If [enableAlias] is `true`, aliases are added to the tasks.
 void addAllTasks({bool enableAlias = false}) {
   addBuildRunnerTask(enableAlias: enableAlias);
+  addServeWebTask(enableAlias: enableAlias);
+  addUpdatePodsTask(enableAlias: enableAlias);
 }
 
+/// Add a task to run build_runner.
 void addBuildRunnerTask({bool enableAlias = false}) {
   buildTaskFunction() =>
       runCommand(command: 'flutter pub run build_runner build -d');
@@ -41,6 +49,94 @@ void addBuildRunnerTask({bool enableAlias = false}) {
         'w',
         description: 'Alias for watch',
         taskFunction: watchTaskFunction,
+      ),
+    );
+  }
+}
+
+/// Add a task to serve the web app.
+void addServeWebTask({bool enableAlias = false}) {
+  serveWebTaskFunction() {
+    final result = Process.runSync('ifconfig', [], runInShell: true);
+    if (result.exitCode != 0) {
+      throw Exception(
+          'ifconfig command failed with exit code ${result.exitCode}');
+    }
+    final output = result.stdout.toString();
+    final lines = output.split('\n');
+    for (var line in lines) {
+      if (line.contains('inet ') && !line.contains('127.0.0.1')) {
+        final ip = line.split(' ')[1];
+        log(ip);
+        runCommand(
+          command:
+              'flutter run -d web-server --web-hostname=$ip --web-port=50505',
+        );
+        break;
+      }
+    }
+  }
+
+  addTask(
+    GrinderTask(
+      'serveWeb',
+      description:
+          'flutter run -d web-server --web-hostname=0.0.0.0 --web-port=50505',
+      taskFunction: serveWebTaskFunction,
+    ),
+  );
+  if (enableAlias) {
+    addTask(
+      GrinderTask(
+        'sw',
+        description: 'Alias for serveWeb',
+        taskFunction: serveWebTaskFunction,
+      ),
+    );
+  }
+}
+
+/// Add a task to update pods.
+void addUpdatePodsTask({bool enableAlias = false}) {
+  updatePodsTaskFunction() {
+    run(
+      'rm',
+      arguments: ['-rf', 'Pods/'],
+      workingDirectory: 'ios',
+    );
+    run(
+      'rm',
+      arguments: ['-rf', 'Podfile.lock'],
+      workingDirectory: 'ios',
+    );
+    run(
+      'flutter',
+      arguments: ['clean'],
+    );
+    run(
+      'flutter',
+      arguments: ['pub', 'get'],
+    );
+    run(
+      'pod',
+      arguments: ['install', '--repo-update'],
+      workingDirectory: 'ios',
+    );
+  }
+
+  addTask(
+    GrinderTask(
+      'updatePods',
+      description: 'Update pods',
+      taskFunction: updatePodsTaskFunction,
+    ),
+  );
+  if (enableAlias) {
+    addTask(
+      GrinderTask(
+        'up',
+        description: 'Alias for updatePods',
+        taskFunction: updatePodsTaskFunction,
       ),
     );
   }
